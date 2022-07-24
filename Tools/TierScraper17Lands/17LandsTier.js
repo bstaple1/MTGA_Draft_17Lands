8dabea1a7d8af7a5188625a5a5b160d3fc49f82d
bstape <testemail@gmail.com>
2022-07-24 15:08:43 -0400

    The TierScraper will now insert the set and the label into the generated json file

--- Tools/TierScraper17Lands/17LandsTier.js
+++ Tools/TierScraper17Lands/17LandsTier.js
@@ -1,6 +1,20 @@
-//var ratingsDict = {"TBD":0.0,"SB":0.0,"F":0.4,"D-":0.8,"D":1.2,"D+":1.5,"C-":1.9,"C":2.3,"C+":2.7,"B-":3.1,"B":3.5,"B+":3.8,"A-":4.2,"A":4.6,"A+":5.0}
-const ratingsDict = {"grid-row: 16 / auto;":0.0,"grid-row: 15 / auto;":0.0,"grid-row: 14 / auto;":0.4,"grid-row: 13 / auto;":0.8,"grid-row: 12 / auto;":1.2,"grid-row: 11 / auto;":1.5,"grid-row: 10 / auto;":1.9,"grid-row: 9 / auto;":2.3,"grid-row: 8 / auto;":2.7,"grid-row: 7 / auto;":3.1,"grid-row: 6 / auto;":3.5,"grid-row: 5 / auto;":3.8,"grid-row: 4 / auto;":4.2,"grid-row: 3 / auto;":4.6,"grid-row: 2 / auto;":5.0}
-        
+const ratingsDict = {"grid-row: 16 / auto;":0.0, //"TBD"
+                     "grid-row: 15 / auto;":0.0, //"SB"
+                     "grid-row: 14 / auto;":0.4, //"F"
+                     "grid-row: 13 / auto;":0.8, //"D-" 
+                     "grid-row: 12 / auto;":1.2, //"D"
+                     "grid-row: 11 / auto;":1.5, //"D+"
+                     "grid-row: 10 / auto;":1.9, //"C-"
+                     "grid-row: 9 / auto;":2.3,  //"C"
+                     "grid-row: 8 / auto;":2.7,  //"C+"
+                     "grid-row: 7 / auto;":3.1,  //"B-"
+                     "grid-row: 6 / auto;":3.5,  //"B"
+                     "grid-row: 5 / auto;":3.8,  //"B+"
+                     "grid-row: 4 / auto;":4.2,  //"A-"
+                     "grid-row: 3 / auto;":4.6,  //"A"
+                     "grid-row: 2 / auto;":5.0}  //"A+"
+const columnPrefix = "tier_text tier_bucket shared "
+const colorIds = ["W","U","B","R","G","M","C","L"]
 
 window.addEventListener('load', function () {
         var grid_zone = document.getElementById("sortable_card_tiers_app")
@@ -24,29 +38,22 @@ function CollectPickRatings (){
                  + currentdate.getSeconds();
     
     var ratingsObj = new Object;
-    ratingsObj.meta = {"collection_date": datetime, "label":"", "set":"", "version": 1.0};
+    var tierLabel = CollectLabel();
+    var setName = CollectSetName();
+    ratingsObj.meta = {"collection_date": datetime, "label": tierLabel, "set": setName, "version": 1.0};
     ratingsObj.ratings = {}
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared W", ratingsObj);
     
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared U", ratingsObj);
-    
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared B", ratingsObj);
-	
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared R", ratingsObj);
-    
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared G", ratingsObj);
-    
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared M", ratingsObj);
-    
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared C", ratingsObj);
-    
-    ratingsObj = CollectColumnRatings("tier_text tier_bucket shared L", ratingsObj);
+    for (var i = 0; i < colorIds.length; i++)
+    {
+        ratingsObj = CollectColumnRatings(colorIds[i], ratingsObj);
+    }
         
     RatingsExport (ratingsObj);
 }
 
-function CollectColumnRatings (columnString, ratingsObj){
+function CollectColumnRatings (columnId, ratingsObj){
 
+    var columnString = columnPrefix + columnId;
 	var tableRows = document.getElementsByClassName(columnString);
     
     for(let i = 0; i < tableRows.length; i++)
@@ -68,17 +75,45 @@ function CollectColumnRatings (columnString, ratingsObj){
     return ratingsObj;
 }
 
+function CollectSetName (){
+    var setName = "";
+    
+    try{
+        let tierListElement = document.getElementById("sortable_card_tiers_app");
+        
+        setName = tierListElement.dataset.expansion;
+    
+    }catch(error){
+        console.log(error);
+    }
+    
+    return setName;
+}
+
+function CollectLabel (){
+    var tierLabel = "";
+    
+    try{
+        //tierLabel = document.getElementsByClassName("tier-container main-text")[0][0].textContent;
+        tierLabel = document.querySelector("h2").textContent;
+    }catch(error){
+        console.log(error);
+    }
+    
+    return tierLabel;
+}
 function RatingsExport (ratingsObj){
 
 	var url = document.URL.split("/");
 
-	var fileName = "Tier_" +  Date.now().toString() + ".json";
+	//var fileName = "Tier_" +  Date.now().toString() + ".json";
+    var filename = `Tier_${ratingsObj.meta.set}_${Date.now().toString()}.json`
 	
 	var _ratingObj = JSON.stringify(ratingsObj , null, 4); //indentation in json format, human readable
 	
 	var vLink = document.createElement('a'),
 	vBlob = new Blob([_ratingObj], {type: "octet/stream"}),
-	vName = fileName;
+	vName = filename;
 	vUrl = window.URL.createObjectURL(vBlob);
 	vLink.setAttribute('href', vUrl);
 	vLink.setAttribute('download', vName );
